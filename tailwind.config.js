1795cf59cf8660c4a1bd152254456f0e0d1f742e
Danitza Bonilla <danitzabonilla@gmail.com>
2020-11-22 14:44:19 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  theme: {
+    fontFamily: {
+      montserrat: ['Montserrat'],
+    },
+    extend: {
+      colors: {
+        primary: '#2d3051',
+      },
+      width: {
+        '2/7': '28.5714286%',
+      }
+    },
+    inset: {
+      0: 0,
+      16: '4rem',
+    },
+  },
+  variants: {},
+  plugins: [],
+};
